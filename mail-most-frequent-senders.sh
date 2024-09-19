f5dc9994049ea2389bebbcf2069d95f556cd7755
Hugo Ferreira <hugocf@users.noreply.github.com>
2024-09-19 12:31:34 +0100

    Handle spaces when grouping the most frequent senders from eml Mail messages

--- mail-most-frequent-senders.sh
+++ mail-most-frequent-senders.sh
@@ -3,5 +3,5 @@
 echo "Filter and group most frequent senders from an exported list of *.eml mail messages from macOS Mail app:"
 folder=${1:-.}
 
-grep "^From:" $folder/*.eml | sed -E 's|.*<(.*)>.*|\1|g' > $folder/senders.txt
-sort $folder/senders.txt | uniq -c | sort -nr | head -40
+grep "^From:" "$folder"/*.eml | sed -E 's|.*<(.*)>.*|\1|g' > "$folder"/senders.txt
+sort "$folder"/senders.txt | uniq -c | sort -nr | head -40
